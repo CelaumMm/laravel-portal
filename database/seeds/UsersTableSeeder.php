09ccbc3d02b1ae71642993d8352dcc5218812ce2
Marcelo Camargo <marcelo.camargo@tvfsoftware.com>
2019-07-29 17:59:34 -0300

    Primeiro commit

---
+++ database/seeds/UsersTableSeeder.php
@@ -0,0 +1,27 @@
+<?php
+
+use App\User;
+use Illuminate\Database\Seeder;
+
+class UsersTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        User::create([
+            'name'      => 'Administrador master',
+            'email'     => 'admin@admin.com',
+            'password'  => bcrypt('123456'),
+        ]);
+
+        User::create([
+            'name'      => 'Usuario comum',
+            'email'     => 'user@user.com',
+            'password'  => bcrypt('123456'),
+        ]);
+    }
+}
