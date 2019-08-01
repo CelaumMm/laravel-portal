b3189a7ec9054c0b279d6120df49cff054c214b4
Marcelo Camargo <marcelo.camargo@tvfsoftware.com>
2019-08-01 16:56:40 -0300

    Add a configuração do adminlte

---
+++ config/adminlte.php
@@ -0,0 +1,175 @@
+<?php
+
+return [
+    /*
+    |--------------------------------------------------------------------------
+    | Locale icon
+    |--------------------------------------------------------------------------
+    |
+    */
+    'icon_pt-BR' => 'flag-icon flag-icon-br',
+    'icon_en' => 'flag-icon flag-icon-us',
+    'icon_es' => 'flag-icon flag-icon-es',
+    /*
+    |--------------------------------------------------------------------------
+    | Footer
+    |--------------------------------------------------------------------------
+    |
+    */
+    'since_year' => '2019',
+    'footer_name' => 'Codegree',
+    'produced_by' => 'Codegree',
+    'produced_url' => 'http://codegree.com.br',
+    'version' => '1.0.0',
+    /*
+    |--------------------------------------------------------------------------
+    | Logo
+    |--------------------------------------------------------------------------
+    |
+    | This logo is displayed at the upper left corner of your admin panel.
+    | You can use basic HTML here if you want. The logo has also a mini
+    | variant, used for the mini side bar. Make it 3 letters or so
+    |
+    */
+    'logo' => '<b>Code</b>gree',
+    'logo_mini' => '<b>C</b>G',
+    /*
+    |--------------------------------------------------------------------------
+    | Skin Color
+    |--------------------------------------------------------------------------
+    |
+    | Choose a skin color for your admin panel. The available skin colors:
+    | blue, black, purple, yellow, red, and green. Each skin also has a
+    | ligth variant: blue-light, purple-light, purple-light, etc.
+    |
+    */
+    'skin' => 'green',
+    /*
+    |--------------------------------------------------------------------------
+    | Layout
+    |--------------------------------------------------------------------------
+    |
+    | Choose a layout for your admin panel. The available layout options:
+    | null, 'boxed', 'fixed', 'top-nav'. null is the default, top-nav
+    | removes the sidebar and places your menu in the top navbar
+    |
+    */
+    'layout' => null,
+    /*
+    |--------------------------------------------------------------------------
+    | Collapse Sidebar
+    |--------------------------------------------------------------------------
+    |
+    | Here we choose and option to be able to start with a collapsed side
+    | bar. To adjust your sidebar layout simply set this  either true
+    | this is compatible with layouts except top-nav layout option
+    |
+    */
+    'collapse_sidebar' => false,
+    /*
+    |--------------------------------------------------------------------------
+    | Menu Items
+    |--------------------------------------------------------------------------
+    |
+    | Specify your menu items to display in the left sidebar. Each menu item
+    | should have a text and and a URL. You can also specify an icon from
+    | Font Awesome. A string instead of an array represents a header in sidebar
+    | layout. The 'can' is a filter on Laravel's built in Gate functionality.
+    |
+    */
+    'menu' => [
+        'MAIN NAVIGATION',
+        [
+            'text' => 'Blog',
+            'url'  => 'admin/blog',
+            'can'  => 'manage-blog',
+        ],
+        [
+            'text'        => 'Pages',
+            'url'         => 'admin/pages',
+            'icon'        => 'file',
+            'label'       => 4,
+            'label_color' => 'success',
+        ],
+        'ACCOUNT SETTINGS',
+        [
+            'text' => 'Profile',
+            'url'  => 'admin/settings',
+            'icon' => 'user',
+        ],
+        [
+            'text' => 'Change Password',
+            'url'  => 'admin/settings',
+            'icon' => 'lock',
+        ],
+        [
+            'text'    => 'Multilevel',
+            'icon'    => 'share',
+            'submenu' => [
+                [
+                    'text' => 'Level One',
+                    'url'  => '#',
+                ],
+                [
+                    'text'    => 'Level One',
+                    'url'     => '#',
+                    'submenu' => [
+                        [
+                            'text' => 'Level Two',
+                            'url'  => '#',
+                        ],
+                        [
+                            'text'    => 'Level Two',
+                            'url'     => '#',
+                            'submenu' => [
+                                [
+                                    'text' => 'Level Three',
+                                    'url'  => '#',
+                                ],
+                                [
+                                    'text' => 'Level Three',
+                                    'url'  => '#',
+                                ],
+                            ],
+                        ],
+                    ],
+                ],
+                [
+                    'text' => 'Level One',
+                    'url'  => '#',
+                ],
+            ],
+        ],
+        'LABELS',
+        [
+            'text'       => 'Important',
+            'icon_color' => 'red',
+        ],
+        [
+            'text'       => 'Warning',
+            'icon_color' => 'yellow',
+        ],
+        [
+            'text'       => 'Information',
+            'icon_color' => 'aqua',
+        ],
+    ],
+    /*
+    |--------------------------------------------------------------------------
+    | Menu Filters
+    |--------------------------------------------------------------------------
+    |
+    | Choose what filters you want to include for rendering the menu.
+    | You can add your own filters to this array after you've created them.
+    | You can comment out the GateFilter if you don't want to use Laravel's
+    | built in Gate functionality
+    |
+    */
+    'filters' => [
+        // JeroenNoten\LaravelAdminLte\Menu\Filters\HrefFilter::class,
+        // JeroenNoten\LaravelAdminLte\Menu\Filters\ActiveFilter::class,
+        // JeroenNoten\LaravelAdminLte\Menu\Filters\SubmenuFilter::class,
+        // JeroenNoten\LaravelAdminLte\Menu\Filters\ClassesFilter::class,
+        // JeroenNoten\LaravelAdminLte\Menu\Filters\GateFilter::class,
+    ],
+];
